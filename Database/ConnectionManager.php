fd00dd0be54fa25167d4fc495d86aece367eafc2
Alan Lima <alima@outlook.com>
2017-07-06 16:43:58 +1000

    fixed the variable visibility at ConnectionManager and renamed files and directories to match with a case sensitve environment

--- Database/ConnectionManager.php
+++ Database/ConnectionManager.php
@@ -13,35 +13,31 @@ class ConnectionManager
 
     public function open()
     {
-        global $db;
-        if (!is_resource($db)) {
+        if (!is_resource($this->db)) {
             /* Conection String eg.: mysqli("localhost", "lpaecomms", "letmein", "lpaecomms")
        *   - Replace the connection string tags below with your MySQL parameters
        */
-      $db = new \mysqli(
-        $db_host, // host
-        $db_login, // user name
-        $db_password, // password
-        $db_name // db name
+      $this->db = new \mysqli(
+        $this->db_host, // host
+        $this->db_login, // user name
+        $this->db_password, // password
+        $this->db_name // db name
       );
-            if ($db->connect_errno) {
-                echo "Failed to connect to MySQL: (" .
-          $db->connect_errno . ") " .
-          $db->connect_error;
+            if ($this->db->connect_errno) {
+                echo "Failed to connect to MySQL: ({$this->db->connect_errno}) {$this->db->connect_error}";
             }
         }
     }
 
     public function query($sql)
     {
-        global $db;
-        return $db->query($sql);
+        return $this->db->query($sql);
     }
 
     public function createLink(){
-      return new \PDO(   'mysql:host=localhost;dbname=LPA_eComms;charset=utf8mb4',
-                          'lpaecomms',
-                          'lpaecomms',
+      return new \PDO(   "mysql:host={$this->db_host};dbname={$this->db_name};charset=utf8mb4",
+                          $this->db_login,
+                          $this->db_password,
                           array(
                               \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
                               \PDO::ATTR_PERSISTENT => false
